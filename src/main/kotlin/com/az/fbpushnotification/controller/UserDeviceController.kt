84cf0dc31afb44511d685b194ed5e8278fb8348a
Jeremiah Udoh <ujeremiah200@gmail.com>
2024-01-31 23:38:36 +0100

    Services and Controllers

--- src/main/kotlin/com/az/fbpushnotification/controller/UserDeviceController.kt
+++ src/main/kotlin/com/az/fbpushnotification/controller/UserDeviceController.kt
@@ -1,4 +1,11 @@
 package com.az.fbpushnotification.controller
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/v1/user-device")
 class UserDeviceController {
+
 }
--- src/main/kotlin/com/az/fbpushnotification/service/UserDeviceService.kt
+++ src/main/kotlin/com/az/fbpushnotification/service/UserDeviceService.kt
@@ -1,4 +1,5 @@
 package com.az.fbpushnotification.service
 
 interface UserDeviceService {
+
 }
